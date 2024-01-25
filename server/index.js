fc131410dd28d5f180c0bd9abcdda44d43f8f457
Faxraddin <reydivonay@gmail.com>
2024-01-25 21:00:13 +0400

    started backend

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotentv = require('dotenv');
+
+const app = express();
+dotentv.config();
+
+app.use(express.json());
+app.use(cors({origin:true,credentials:true}));
+app.use(express.urlencoded({extended:true}));
+
+const DB = process.env.URL;
+const PORT = 8000;
+
+mongoose.connect(DB).catch((err)=>console.log('Error' + err));
+app.listen(PORT,()=>console.log('Listeting on port' + PORT))
