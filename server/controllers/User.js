4dc5c679068caff3edb88fa04372300e860db77c
Faxraddin <reydivonay@gmail.com>
2024-02-11 22:15:46 +0400

    Login and SignUp

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -3,7 +3,61 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
 import User from "../models/User";
-
-
-
 dotenv.config();
+
+// Sign Up
+
+const SignUp = async (req,res) => {
+    const { name, email, password } = req.body;
+
+    try {
+      // Check if the user already exists
+      const existUser = User.findOne({ email });
+      if (existUser) {
+        return res.status(401).json({ message: "Email already taken!" });
+      }
+
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create a new user
+      const newUser = new User({
+        name,
+        email,
+        password:hashedPassword
+      });
+
+      await newUser.save();
+
+      return res.status(200).json({message:'User registered successfully!'})
+    } catch (error) {
+        res.status(500).json({ message: "Error with Login" });
+    }
+};
+
+// Sign In
+
+const Login = async (req, res) => {
+  try {
+    const {email,password} = req.body;
+
+    const user = await User.findOne({email});
+    if (!user) {
+        return res.status(401).json({message:'No such user :('});
+    } else {
+        const compare = await bcrypt.compare(password,user.password);
+        if (!compare) {
+            return res.status(401).json({message:'Wrong password!'});
+        };
+
+        return res.status(200).json({message:'Welcome Back!',user:user})
+    }
+  } catch (error) {
+    res.status(500).json({message:'Error with Login'})
+  }
+};
+
+module.exports = {
+    Login:Login,
+    SignUp:SignUp,
+}
--- server/routes/User.js
+++ server/routes/User.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {} = require('../controllers');
+const {Login,SignUp} = require('../controllers/User');
 
-
+router.post('Login',Login);
+router.post('SignUp',SignUp)
 
 module.exports = router
