f0b2d0742e01640e6e9adfe7e2e299486a4c2d9c
Faxraddin <reydivonay@gmail.com>
2024-02-12 23:14:21 +0400

    design schema

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const BookSchema = new mongoose.Schema({
+const bookSchema = new mongoose.Schema({
   author: {
     type: String,
     required: true,
@@ -13,4 +13,15 @@ const BookSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  category: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
+
+module.exports = mongoose.model('Book',bookSchema)
