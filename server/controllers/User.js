f5a154d321f6761a1ac791f80c8bb198be368693
Faxraddin <reydivonay@gmail.com>
2024-02-12 23:23:06 +0400

    GetBooksForUser

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -58,7 +58,6 @@ const Login = async (req, res) => {
 };
 
 // Add book
-
 const AddBook = async (req,res) => {
   try {
     const { author, title, text, category, userId } = req.body;
@@ -80,9 +79,23 @@ const AddBook = async (req,res) => {
   }
 }
 
+//GetBooksForUser
+const GetBooksForUser = async (req, res) => {
+  try {
+    const {userId} = req.body;
+
+    const booksForUser = await Book.find({user: userId});
+
+    res.status(200).json({ books: booksForUser });
+  } catch (error) {
+    res.status(500).json({ message: "Error GetBooksForUser" });
+  }
+}
+
 module.exports = {
     Login:Login,
     SignUp:SignUp,
 
     AddBook:AddBook,
+    GetBooksForUser:GetBooksForUser,
 }
